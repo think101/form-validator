cfdca45d676a36fbf29e1d520e0f9bb5c2eac476
think101 <taohuayuan2000@hotmail.com>
2022-08-18 21:40:21 -0700

    add passwd matching check

--- script.js
+++ script.js
@@ -51,6 +51,13 @@ function checkLength(input, min, max) {
     }
 }
 
+// check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if(input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+    }    
+}
+
 function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
@@ -62,5 +69,6 @@ form.addEventListener('submit', function(e) {
     checkLength(username, 3, 15);
     checkLength(password, 6, 25);
     checkEmail(email);
+    checkPasswordsMatch(password, password2);
 });
 
